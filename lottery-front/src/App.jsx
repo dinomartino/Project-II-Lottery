451754388f9f8cb1b074b3eaa608b40cd6c80910
dinomartino <1155193770@link.cuhk.edu.hk>
2025-06-11 17:54:55 +0800

    add Connect Button

--- lottery-front/src/App.jsx
+++ lottery-front/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import ConnectButton from "./components/ConnectButton";
+import Web3 from "web3";
 
 function App() {
-  return(
-    <></>
-  )
+  return (
+    <main>
+      <div className="bg-black w-full h-screen bg-center bg-cover absolute z-0">
+        <h1 className="text-white">Lottery Dapp</h1>
+        <ConnectButton />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ lottery-front/src/components/ConnectButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Web3 from "web3";
+
+function ConnectButton() {
+  async function connect() {
+    try {
+      if (!window.ethereum) {
+        throw new Error("MetaMask is not install");
+      }
+
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = accounts[0];
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div>
+      <button className="text-cyan-300 flex flex-col" onClick={connect}>
+        Connect
+      </button>
+    </div>
+  );
+}
+
+export default ConnectButton;
