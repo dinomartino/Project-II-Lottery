b915850ab2f29c17abb11cb0fc4511439ee24d8c
dinomartino <1155193770@link.cuhk.edu.hk>
2025-06-13 19:06:13 +0800

    setup web3.js

--- lottery-front/src/App.jsx
+++ lottery-front/src/App.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Web3 from "web3";
 
 function App() {
+  const [Web3, setWeb3] = useState();
+
+  async function initialiseWeb3() {
+    setWeb3(new window.Web3(window.ethereum));
+  }
+
   return (
     <main>
       <div>
