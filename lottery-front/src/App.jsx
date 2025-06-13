347b26925e592e89ecec7c864fa236c2ecf36abc
dinomartino <1155193770@link.cuhk.edu.hk>
2025-06-13 23:54:09 +0800

    update web3

--- lottery-front/src/App.jsx
+++ lottery-front/src/App.jsx
@@ -1,19 +1,20 @@
 import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
+import IntroParagragh from "./components/IntroParagraph";
 import Web3 from "web3";
+import { abi, contractAddress } from "./contractInfo";
 
 function App() {
-  const [Web3, setWeb3] = useState();
-
   async function initialiseWeb3() {
-    setWeb3(new window.Web3(window.ethereum));
+    const web3 = new Web3(window.ethereum);
   }
+  initialiseWeb3();
 
   return (
     <main>
-      <div>
-        <Navbar />
-      </div>
+      <Navbar />
+      <br />
+      <IntroParagragh />
     </main>
   );
 }
